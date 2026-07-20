2a758f45a8fe8afd8be9d6096cb1f432a05c84ad
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-07-20 11:03:23 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,9 +15,13 @@ app.get('/api/status', (req, res) => {
   res.status(200).json({ status: 'online', mensagem: 'API NexusLog rodando com sucesso! 🚀' });
 });
 
-// Importa e pluga o arquivo de rotas que configuramos acima
+// Importa e pluga as rotas antigas
 const solicitacoesRoutes = require('./src/routes/solicitacoesRoutes');
 app.use('/api/solicitacoes', solicitacoesRoutes);
+
+// 👇 ADICIONA ESTAS DUAS LINHAS AQUI PARA O ESTOQUE 👇
+const estoqueRoutes = require('./src/routes/estoqueRoutes');
+app.use('/api/estoque', estoqueRoutes);
 
 // INICIALIZAÇÃO DO SERVIDOR
 app.listen(PORT, () => {
